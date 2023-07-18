bfba0fe7e4c8e96d03d71be1f427fc39092a016e
practicasIntermedio <doscaras51@gmail.com>
2023-07-18 17:34:28 -0500

    cambios hechos #1

--- Ejercicio16React/src/App.jsx
+++ Ejercicio16React/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import YouTube from 'react-youtube';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_URL = 'https://api.themoviedb.org/3';
+  const API_KEY = '621dd4ce3cbd8016e5b77c0f3bc44edc';
+  const image_path = 'https://image.tmdb.org/t/p/original';
+  const URL_image = 'https://image.tmdb.org/t/p/original'
+
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState('');
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({title: "Loading Movies"});
+  const [playing, setPlaying] = useState(false)
+
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const { data: { results } } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+
+    setMovies(results);
+    setMovie(results[0]);
+  };
+
+  useEffect(() => {
+    fetchMovies(searchKey);
+  }, [searchKey]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="container mt-3">
+        <div className="row">
+          {movies.map((movie) => (
+            <div key={movie.id} className="col-md-4 mb-3">
+              <img src={`${URL_image}${movie.poster_path}`} alt="" height={600} width='100%'/>
+              <h4 className="text-center">{movie.title}</h4>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
