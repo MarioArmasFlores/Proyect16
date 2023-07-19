d3152419855252b511b7864c7ad8a3d72668984e
practicasIntermedio <doscaras51@gmail.com>
2023-07-19 14:31:00 -0500

    segundoCommit#16

--- Ejercicio16React/src/App.jsx
+++ Ejercicio16React/src/App.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; //importacion de los hookers
 import axios from "axios";
-import YouTube from 'react-youtube';
 
 function App() {
-  const API_URL = 'https://api.themoviedb.org/3';
-  const API_KEY = '621dd4ce3cbd8016e5b77c0f3bc44edc';
-  const image_path = 'https://image.tmdb.org/t/p/original';
-  const URL_image = 'https://image.tmdb.org/t/p/original'
 
-  const [movies, setMovies] = useState([]);
-  const [searchKey, setSearchKey] = useState('');
-  const [trailer, setTrailer] = useState(null);
+  //constantes y variables.
+  const API_URL = 'https://api.themoviedb.org/3'; 
+  const API_KEY = '621dd4ce3cbd8016e5b77c0f3bc44edc';
+  const URL_image = 'https://image.tmdb.org/t/p/original';
+
+  //constantes de estados useState.
+  
+  const [movies, setMovies] = useState([]); //este hook de movies (plural) recibe y almacena en un array todas las peliculas a travez de la peticion fetch
+  const [searchKey, setSearchKey] = useState(''); 
   const [movie, setMovie] = useState({title: "Loading Movies"});
-  const [playing, setPlaying] = useState(false)
+  const [showDescription, setShowDescription] = useState(false);
+  const [selectedMovieId, setSelectedMovieId] = useState(null);
 
   const fetchMovies = async (searchKey) => {
-    const type = searchKey ? "search" : "discover";
+    const type =  "discover";
     const { data: { results } } = await axios.get(`${API_URL}/${type}/movie`, {
       params: {
         api_key: API_KEY,
@@ -26,6 +27,33 @@ function App() {
 
     setMovies(results);
     setMovie(results[0]);
+
+    if (results.length) {
+      await fetchMovie(results[0].id);
+    }
+  };
+
+  // Petición de información de la película seleccionada
+  const fetchMovie = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "description"
+      }
+    });
+
+    setMovie(data);
+  };
+
+  const selectMovie = (movie) => {
+    if (selectedMovieId === movie.id) {
+      setSelectedMovieId(null); // Desmarca la película seleccionada si se hace clic nuevamente
+    } else {
+      setSelectedMovieId(movie.id); // Establece el ID de la película seleccionada
+      fetchMovie(movie.id);
+      setMovie(movie);
+      setShowDescription(true); // Muestra la descripción para la nueva película seleccionada
+    }
   };
 
   useEffect(() => {
@@ -38,8 +66,13 @@ function App() {
         <div className="row">
           {movies.map((movie) => (
             <div key={movie.id} className="col-md-4 mb-3">
-              <img src={`${URL_image}${movie.poster_path}`} alt="" height={600} width='100%'/>
+              <img src={`${URL_image}${movie.poster_path}`} alt="" height={600} width="100%" onClick={() => selectMovie(movie)} />
               <h4 className="text-center">{movie.title}</h4>
+              {selectedMovieId === movie.id && (
+                <div>
+                  {movie.overview}
+                </div>
+              )}
             </div>
           ))}
         </div>
